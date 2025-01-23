318917f98803d64cbfd2c5688ead8f15bca5d71c
Anato-Eini <jameskennethacabal@gmail.com>
2025-01-23 14:08:33 +0800

    Added solution for the s03 activity

--- s03/activity/index.js
+++ s03/activity/index.js
@@ -1,21 +1,27 @@
+/*
+1. In the s03 folder, create an activity folder, an index.html file inside of it and link the index.js file.
+2. Create an index.js file and console log the message "Hello World" to ensure that the script file is properly associated with the html file.
+3. Copy the activity code from your Instructor. Paste the activity code to your index.js file.
+ */
+
+// console.log("Hello World");
+
+/*
+4. Create the following variables to store the following user details:
+
+    - Variable Name:
+        - firstName
+        - lastName
+        - age
+        - mobileNumber
+        - address
+*/
+
+/*
+5. Identify and implement the best practices of creating and using variables by avoiding errors and debugging the following codes. 
+    - Log the values of each variable to follow/mimic the output.
+*/
 console.log("Hello World");
-console.log("This is our first JS session!")
-
-
-let myName = "James";
-
-console.log(myName);
-
-let newVariable;
-console.log(newVariable);
-
-
-console.log(hello);
-var hello = "WORLD"
-
-
-console.log("User Profile");
-
 
 let firstName = "James Kenneth";
 let lastName = "Acabal";
@@ -29,3 +35,26 @@ console.log(age);
 console.log(mobileNumber);
 console.log(address);
 
+let realName = "Steve Rogers";
+console.log("My real name is:");
+console.log(realName);
+
+let currentAge = true;
+console.log("My current age is: ");
+console.log(currentAge);
+
+let interestRate = 30;
+console.log("The current interest rate: ");
+console.log(interestRate);
+
+let x = "EcommerceAPI";
+console.log("The current login key is: ");
+console.log(x);
+
+let bankCode = "BANK123";
+console.log("Bank code: ");
+console.log(bankCode);
+
+let bankBalance = 500;
+console.log("Current bank balance: ");
+console.log(bankBalance);
