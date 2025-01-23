e7d1fb134cdd074e72d9b30fc5ec4801a313f1cb
Anato-Eini <jameskennethacabal@gmail.com>
2025-01-23 14:00:55 +0800

    Discussion S03

--- s03/discussion/index.js
+++ s03/discussion/index.js
@@ -1,1 +1,15 @@
 console.log("Hello World");
+console.log("This is our first JS session!")
+
+
+let myName = "James";
+
+console.log(myName);
+
+let newVariable;
+console.log(newVariable);
+
+
+console.log(hello);
+var hello = "WORLD"
+
