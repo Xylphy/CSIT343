e3553b15aa0e5e8a14f174db54ebbcc06d1b3166
Anato-Eini <jameskennethacabal@gmail.com>
2025-01-23 14:04:52 +0800

    Added solution for the s03 activity

---
+++ s03/activity/index.js
@@ -0,0 +1,31 @@
+console.log("Hello World");
+console.log("This is our first JS session!")
+
+
+let myName = "James";
+
+console.log(myName);
+
+let newVariable;
+console.log(newVariable);
+
+
+console.log(hello);
+var hello = "WORLD"
+
+
+console.log("User Profile");
+
+
+let firstName = "James Kenneth";
+let lastName = "Acabal";
+let age = 20;
+let mobileNumber = "09123456789";
+let address = "Cebu City";
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(mobileNumber);
+console.log(address);
+
