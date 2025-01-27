c0490db8ea8f60de7bca80ac4cfad43c1b5519ec
Anato-Eini <jameskennethacabal@gmail.com>
2025-01-27 18:38:30 +0800

    Discussion S03

--- s03/discussion/index.js
+++ s03/discussion/index.js
@@ -1,15 +1,48 @@
-console.log("Hello World");
+console.log("Hello World!");
 console.log("This is our first JS session!")
 
+// Comments
+	// ctrl + /
+	// to describe the written codes.
+	// Documentation:
+		// code readability
+		// maintainability
+		// collaboration
 
-let myName = "James";
+		/*
+			These are two types of comments:
+			1. single-line comments denoted by two slashes
+			2. multi-line comment denote by a slash and asterisk (ctrl + shift + /)
+		*/
 
-console.log(myName);
+console.log("Tolits");
+console.log("Tolits");
+console.log("Tolits");
 
-let newVariable;
-console.log(newVariable);
+// Variables
+	// to contain data within a program.
+	
+	// Variable Declaration w/ Initialization
+	let myName = "Tolits";
 
+	console.log(myName);
+	
 
-console.log(hello);
-var hello = "WORLD"
+	// Variable Declaration
+	let newVariable;
+	console.log(newVariable);
 
+	// we use assignment(=) operator to assign/reassign a value
+	let myPet = "Bantay";
+	console.log(myPet);
+
+	// Reference error - not defined 
+	// console.log(hello);
+	// let hello;
+
+	// Declare the variable first before using it.
+	// let hello;
+	// console.log(hello);
+
+	// console.log(hello);
+	// var hello = "greeting";
