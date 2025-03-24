56f20e7d609804af7d3696821512813d0f983c69
❄ Fleur de Blue <jameskennethacabal@gmail.com>
2025-03-24 13:47:54 +0800

    Added Discussion

---
+++ s24/discussion/index.js
@@ -0,0 +1,91 @@
+console.log("Hello World");
+
+// [SECTION] Arrow Function
+// Compact aleternative syntax to traditional functions
+// "DRY" - Don't Repeat Yourself
+
+const hello = () => {
+    console.log("Hello World");
+};
+
+// Function expression
+// What is the difference between function declaration and function expression?
+// Function declaration can be called before the function is defined
+// Function expression can only be called after the function is defined
+
+// exprFunction(); // This will not work
+
+let exprFunction = function () {
+    console.log("Hello from the other side");
+};
+
+exprFunction(); // This will work
+
+// Arrow Functions with Template Literals
+
+let printFullName = (firstName, middleInitial, lastName) => {
+    console.log(`Hello ${firstName} ${middleInitial} ${lastName}`);
+};
+
+printFullName("John", "Doe", "Smith");
+
+// Implicit Return Statement
+// Arrow functions are able to return values without the use of "return" keyword
+// If arrow function return are only written in one line and does not explicit code block with {};
+
+// pre-arrow function without implicit return
+let add = (a, b) => {
+    return a + b;
+};
+
+let total = add(4, 5);
+console.log(total);
+
+add = (a, b) => a + b;
+
+let sum = add(2, 2);
+console.log(sum);
+
+subtract = (a, b) => a - b;
+let difference = subtract(10, 5);
+console.log(difference);
+
+// Arrow Functions in Array Methods
+// shorten the way to write array methods that require anonymous functions.
+
+let playlist1 = ["Signal", "What is love?", "Can't stop me", "Feel Special"];
+
+playlist1.forEach((song) => {
+    console.log(song);
+});
+
+let sales = [8900, 2000, 1000, 5000];
+let isAllNumbers = sales.every((sale) => typeof sale === "number");
+
+console.log(isAllNumbers);
+
+// [SECTION] Class-based objects blueprints
+// Allows create/instantiate objects using classes as blueprints.
+// Creating a class blueprint
+
+class Car {
+    constructor(brand, name, year) {
+        this.brand = brand;
+        this.name = name;
+        this.year = year;
+    }
+}
+
+// Instantiating an object
+const myCar = new Car();
+console.log(myCar);
+
+myCar.brand = "Toyota";
+myCar.name = "Vios";
+myCar.year = 2021;
+
+console.log(myCar);
+
+// Create a new object from car class with initialized values:
+const myNewCar = Car("Toyota", "Vios", 2021);
+console.log(myNewCar);
